24b07dbe51471911e5d030c3d3e7730a26d306ad
tmadar <travismadar@gmail.com>
2013-12-04 22:51:22 -0700

    forgot jquery.js

--- terminator.js
+++ terminator.js
@@ -1,3 +1,5 @@
-$(".channel-main").css("background", "black");
-$("#right_col").css("background", "black");
-$(".chat_line").css("color", "white");
+chrome.extension.sendRequest({'url': 'http://twitch.tv/*'}, function(data) {
+	$(".channel-main").css("background", "black");
+	$("#right_col").css("background", "black");
+	$(".chat_line").css("color", "white");
+});
